f13cd53939b4382ea1ea89728c1cf90205f34fd1
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-02-18 17:25:34 +0530

    Basic Cpp

---
+++ Basic cpp/02  WAP to check the Size of Various Data Type.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+   cout<<"============================================"<< endl;
+   cout<<"Size of char : " << sizeof(char) << endl;
+   cout<<"============================================"<< endl;
+   cout<<"Size of int : " << sizeof(int) << endl;
+   cout<<"============================================"<< endl;
+   cout<<"Size of float : " << sizeof(float) << endl;
+   cout<<"============================================"<< endl;
+   cout<<"Size of double : " << sizeof(double) << endl;
+   cout<<"============================================"<< endl;
+   cout<<"Size of short int : " << sizeof(short int) << endl;
+   cout<<"============================================"<< endl;
+   cout<<"Size of long int : " << sizeof(long int) << endl;
+   cout<<"============================================"<< endl;
+   cout<<"Size of wchar_t : " << sizeof(wchar_t) << endl;
+   cout<<"============================================"<< endl;
+}
